ca4ddd8caf39704ca0213e7f6b492dd39055fb04
Seungmin Lim <seungmin9066@gmail.com>
2021-11-11 21:56:19 +0900

    add 2 til and DeZero basic framework

---
+++ 06. DL/DeZero/DeZero.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+
+class Variable:
+    def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{}은(는) 지원하지 않습니다.'. format(type(data)))
+        
+        self.data = data
+        self.grad = None
+        self.creator = None
+    
+    def set_creator(self, func):
+        self.creator = func
+        
+    def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = [self.creator]  # 1. 함수를 가져온다.
+        while funcs:
+            f = funcs.pop()
+            x, y = f.input, f.output
+            x.grad = f.backward(y.grad)
+            if x.creator is not None:
+                funcs.append(x.creator)
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(as_array(y))
+        output.set_creator(self) # 출력변수에 창조자 설정
+        self.input = input
+        self.output = output # 출력값도 저장
+        return output
+    
+    def forward(self, x):
+        raise NotImplementedError()
+        
+    def backward(self, gy):
+        raise NotImplementedError()
