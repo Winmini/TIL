ce51f7ffd23632201d8b1b6215a82298e2754aef
Seungmin Lim <seungmin9066@gmail.com>
2021-11-28 18:00:46 +0900

    [추가] DeZero 패키지

---
+++ 06. DL/DeZero/__init__.py
@@ -0,0 +1,14 @@
+from dezero.core import Variable
+from dezero.core import Function
+from dezero.core import using_config
+from dezero.core import no_grad
+from dezero.core import as_array
+from dezero.core import as_variable
+from dezero.core import setup_variable
+
+import dezero.functions
+import dezero.utils
+
+
+setup_variable()
+__version__ = '0.0.13'
