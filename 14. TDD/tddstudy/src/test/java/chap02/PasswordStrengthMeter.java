9a2ebc699dd2a791df0bc755ca319db81a9d52c1
Seungmin Lim <seungmin9066@gmail.com>
2022-02-02 01:51:20 +0900

    [추가] 암호검사기 테스트 코드 추가

---
+++ 14. TDD/tddstudy/src/test/java/chap02/PasswordStrengthMeter.java
@@ -0,0 +1,21 @@
+package chap02;
+
+public class PasswordStrengthMeter {
+
+	private static final int PASSWORD_LENGTH_STANDARD = 8;
+	private static final String CONTAINING_NUMBER_REGEX = "^[^0-9]*$";
+
+	public PasswordStrength meter(String password) {
+		if (meterPasswordLength(password) || meterContainingNumber(password))
+			return PasswordStrength.NORMAL;
+		return PasswordStrength.STRONG;
+	}
+
+	private boolean meterPasswordLength(String password) {
+		return password.length() < PASSWORD_LENGTH_STANDARD;
+	}
+
+	private boolean meterContainingNumber(String password) {
+		return password.matches(CONTAINING_NUMBER_REGEX);
+	}
+}
