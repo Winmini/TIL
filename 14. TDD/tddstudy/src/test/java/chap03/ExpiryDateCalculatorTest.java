a8197cffb84d587c05c02df765d32917504277b4
Seungmin Lim <seungmin9066@gmail.com>
2022-02-02 16:52:54 +0900

    [추가] 납부일자가 다른 예외상황 검증코드 추가

--- 14. TDD/tddstudy/src/test/java/chap03/ExpiryDateCalculatorTest.java
+++ 14. TDD/tddstudy/src/test/java/chap03/ExpiryDateCalculatorTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 public class ExpiryDateCalculatorTest {
 
 	private void assertExpiryDate(LocalDate billingDate, int payAmount, LocalDate expectedExpiryDate) {
-		ExpiryDateCalculator  calculator = new ExpiryDateCalculator();
+		ExpiryDateCalculator calculator = new ExpiryDateCalculator();
 		LocalDate realExpiryDate = calculator.calculateExpiryDate(billingDate, payAmount);
 		assertEquals(expectedExpiryDate, realExpiryDate);
 	}
@@ -20,5 +20,13 @@ public class ExpiryDateCalculatorTest {
 			LocalDate.of(2019, 4, 1));
 		assertExpiryDate(LocalDate.of(2020, 5, 5), 10_000,
 			LocalDate.of(2020, 6, 5));
+	}
+
+	@Test
+	void 납부일과_한달_뒤_일자가_같지_않음() {
+		assertExpiryDate(LocalDate.of(2019, 1, 31), 10_000,
+			LocalDate.of(2019, 2, 28));
+		assertExpiryDate(LocalDate.of(2019, 5, 31), 10_000,
+			LocalDate.of(2019, 6, 30));
 	}
 }
