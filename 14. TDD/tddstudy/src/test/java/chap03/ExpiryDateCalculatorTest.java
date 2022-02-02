2723f029111fb8c2ab6fef23c7425ad856f56b87
Seungmin Lim <seungmin9066@gmail.com>
2022-02-02 16:49:52 +0900

    [리팩터링] 중복코드 메서드로 추출

--- 14. TDD/tddstudy/src/test/java/chap03/ExpiryDateCalculatorTest.java
+++ 14. TDD/tddstudy/src/test/java/chap03/ExpiryDateCalculatorTest.java
@@ -8,22 +8,17 @@ import org.junit.jupiter.api.Test;
 
 public class ExpiryDateCalculatorTest {
 
+	private void assertExpiryDate(LocalDate billingDate, int payAmount, LocalDate expectedExpiryDate) {
+		ExpiryDateCalculator  calculator = new ExpiryDateCalculator();
+		LocalDate realExpiryDate = calculator.calculateExpiryDate(billingDate, payAmount);
+		assertEquals(expectedExpiryDate, realExpiryDate);
+	}
+
 	@Test
 	void 만원_납부하면_한달_뒤가_만료일이_됨() {
-		LocalDate billingDate = LocalDate.of(2019, 3, 1);
-		int payAmount = 10_000;
-
-		ExpiryDateCalculator calculator = new ExpiryDateCalculator();
-		LocalDate expiryDate = calculator.calculateExpiryDate(billingDate, payAmount);
-
-		assertEquals(LocalDate.of(2019, 4, 1), expiryDate);
-
-		LocalDate billingDate2 = LocalDate.of(2020, 5, 5);
-		int payAmount2 = 10_000;
-
-		ExpiryDateCalculator calculator2 = new ExpiryDateCalculator();
-		LocalDate expiryDate2 = calculator.calculateExpiryDate(billingDate2, payAmount2);
-
-		assertEquals(LocalDate.of(2020, 6, 5), expiryDate2);
+		assertExpiryDate(LocalDate.of(2019, 3, 1), 10_000,
+			LocalDate.of(2019, 4, 1));
+		assertExpiryDate(LocalDate.of(2020, 5, 5), 10_000,
+			LocalDate.of(2020, 6, 5));
 	}
 }
