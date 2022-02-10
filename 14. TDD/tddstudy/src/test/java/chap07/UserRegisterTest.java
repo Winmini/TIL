27a56a22d2efc9da73c9379abb9cd548da173ecd
Seungmin Lim <seungmin9066@gmail.com>
2022-02-10 22:50:18 +0900

    [추가] 약한 암호 확인 기능을 위한 테스트코드 기반 추가

---
+++ 14. TDD/tddstudy/src/test/java/chap07/UserRegisterTest.java
@@ -0,0 +1,27 @@
+package chap07;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class UserRegisterTest {
+	private UserRegister userRegister;
+	private final StubWeakPasswordChecker stubPasswordChecker = new StubWeakPasswordChecker();
+
+	@BeforeEach
+	void setUp() {
+		userRegister = new UserRegister(stubPasswordChecker);
+	}
+
+	@DisplayName("약한 암호면 가입 실패")
+	@Test
+	void weakPassword() {
+		stubPasswordChecker.setWeak(true);
+
+		assertThrows(WeakPasswordException.class, () -> {
+			userRegister.register("id", "pw", "email");
+		});
+	}
+}
