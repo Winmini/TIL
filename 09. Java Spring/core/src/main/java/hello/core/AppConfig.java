23858719277c332b2b2adf9431d286103bca7e1f
Seungmin Lim <seungmin9066@gmail.com>
2022-02-19 12:32:41 +0900

    [수정] 순수한 자바코드에서 스프링으로 변경

--- 09. Java Spring/core/src/main/java/hello/core/AppConfig.java
+++ 09. Java Spring/core/src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
@@ -8,20 +11,26 @@ import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
+
+@Configuration
 public class AppConfig {
 
+	@Bean
 	public MemberService memberService() {
 		return new MemberServiceImpl(memberRepository());
 	}
 
-	private MemoryMemberRepository memberRepository() {
+	@Bean
+	public MemoryMemberRepository memberRepository() {
 		return new MemoryMemberRepository();
 	}
 
+	@Bean
 	public OrderService orderService() {
 		return new OrderServiceImpl(memberRepository(), discountPolicy());
 	}
 
+	@Bean
 	public DiscountPolicy discountPolicy() {
 		return new FixDiscountPolicy();
 	}
--- 09. Java Spring/core/src/main/java/hello/core/MemberApp.java
+++ 09. Java Spring/core/src/main/java/hello/core/MemberApp.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import org.springframework.boot.web.reactive.context.AnnotationConfigReactiveWebApplicationContext;
+import org.springframework.context.ApplicationContext;
+
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
@@ -7,8 +10,9 @@ import hello.core.member.MemberService;
 public class MemberApp {
 
 	public static void main(String[] args) {
-		AppConfig appConfig = new AppConfig();
-		MemberService memberService = appConfig.memberService();
+
+		ApplicationContext h = new AnnotationConfigReactiveWebApplicationContext(AppConfig.class);
+		MemberService memberService = h.getBean("memberService", MemberService.class);
 
 		Member member = new Member(1L, "memberA", Grade.VIP);
 		memberService.join(member);
