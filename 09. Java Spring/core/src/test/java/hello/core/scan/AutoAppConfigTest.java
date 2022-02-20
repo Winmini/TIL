55c7a48cdeec40d17ae8378e674aa6d03533daf8
Seungmin Lim <seungmin9066@gmail.com>
2022-02-20 12:32:46 +0900

    [추가] 컴포넌트 스캔을 적용하는 코드

--- 09. Java Spring/core/src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ 09. Java Spring/core/src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -1,2 +1,21 @@
-package hello.core.scan;public class AutoAppConfigTest {
+package hello.core.scan;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import hello.core.AutoAppConfig;
+import hello.core.member.MemberService;
+
+public class AutoAppConfigTest {
+
+	@Test
+	void basicScan() {
+		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(
+			AutoAppConfig.class);
+
+		MemberService bean = ac.getBean(MemberService.class);
+		assertThat(bean).isInstanceOf(MemberService.class);
+	}
 }
