7b15c720c4b5e534096bef0f3bb302241a8d5aed
Seungmin Lim <seungmin9066@gmail.com>
2022-02-28 00:07:33 +0900

    [추가] 프록시 코드 추가

--- 16. JPA/jpa-basic/src/main/java/jpabook/jpashop/JpaMain.java
+++ 16. JPA/jpa-basic/src/main/java/jpabook/jpashop/JpaMain.java
@@ -5,13 +5,14 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+import jpabook.jpashop.domain.Member;
+
 public class JpaMain {
 	public static void main(String[] args) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpashop");
 		EntityManager em = emf.createEntityManager();
 		EntityTransaction tx = em.getTransaction();
 		tx.begin();
-
 		try {
 			tx.commit();
 		} catch (Exception e){
