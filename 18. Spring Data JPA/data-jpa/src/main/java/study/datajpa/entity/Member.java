f5f9000261f67defb9cc582c2da1cfdb25713551
Seungmin Lim <seungmin9066@gmail.com>
2022-03-28 21:41:15 +0900

    [추가] 순수 JPA의 기본적인 CRUD 코드 추가

--- 18. Spring Data JPA/data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ 18. Spring Data JPA/data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -1,13 +1,20 @@
 package study.datajpa.entity;
 
+import static javax.persistence.FetchType.*;
 import static lombok.AccessLevel.*;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Getter
@@ -16,10 +23,29 @@ public class Member {
 
 	@Id
 	@GeneratedValue
+	@Column(name = "member_id")
 	private Long id;
 	private String username;
+	private int age;
+
+	@ManyToOne(fetch = LAZY)
+	@JoinColumn(name = "team_id")
+	private Team team;
+
+	public Member(String username, int age, Team team) {
+		this.username = username;
+		this.age = age;
+		if (team != null) {
+			changeTeam(team);
+		}
+	}
 
 	public Member(String username) {
 		this.username = username;
+	}
+
+	public void changeTeam(Team team) {
+		this.team = team;
+		team.getMembers().add(this);
 	}
 }
