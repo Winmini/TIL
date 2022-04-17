8e9bedc621b961a5bb1d2cdbc82f1c464a571050
Seungmin Lim <seungmin9066@gmail.com>
2022-04-17 22:39:13 +0900

    [추가] 트랜잭션을 공부하는 코드 추가

--- 19. Spring DB/jdbc/src/main/java/hello/jdbc/service/MemberServiceV2.java
+++ 19. Spring DB/jdbc/src/main/java/hello/jdbc/service/MemberServiceV2.java
@@ -14,38 +14,38 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RequiredArgsConstructor
-public class MemberServiceV2 {
+public class MemberServiceV2 {	public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+	Connection con = dataSource.getConnection();
+	try{
+		con.setAutoCommit(false); // 트랜잭션 시작
+		//비즈니스 로직 수행
+		Member fromMember = memberRepository.findById(con, fromId);
+		Member toMember = memberRepository.findById(con, toId);
+
+		memberRepository.update(con, fromId, fromMember.getMoney() - money);
+		validate(toMember);
+		memberRepository.update(con, toId, toMember.getMoney() + money);
+
+		con.commit();
+	}catch (Exception e){
+		con.rollback();
+		throw new IllegalStateException(e);
+	}finally {
+		if(con != null){
+			try{
+				con.setAutoCommit(true); // 커넥션 풀을 고려
+				con.close();
+			}catch (Exception e){
+				log.error("error", e);
+			}
+		}
+	}
+}
 
 	private final MemberRepositoryV2 memberRepository;
 	private final DataSource dataSource;
 
-	public void accountTransfer(String fromId, String toId, int money) throws SQLException {
-		Connection con = dataSource.getConnection();
-		try{
-			con.setAutoCommit(false); // 트랜잭션 시작
-			//비즈니스 로직 수행
-			Member fromMember = memberRepository.findById(con, fromId);
-			Member toMember = memberRepository.findById(con, toId);
 
-			memberRepository.update(con, fromId, fromMember.getMoney() - money);
-			validate(toMember);
-			memberRepository.update(con, toId, toMember.getMoney() + money);
-
-			con.commit();
-		}catch (Exception e){
-			con.rollback();
-			throw new IllegalStateException(e);
-		}finally {
-			if(con != null){
-				try{
-					con.setAutoCommit(true); // 커넥션 풀을 고려
-					con.close();
-				}catch (Exception e){
-					log.error("error", e);
-				}
-			}
-		}
-	}
 
 	private void validate(Member toMember) {
 		if(toMember.getMemberId().equals("ex")){
