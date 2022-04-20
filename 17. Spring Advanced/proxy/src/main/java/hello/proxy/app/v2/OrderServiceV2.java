9b42c2b6bd9626a2c99c2ab07d7bf3f842b0aed3
Seungmin Lim <seungmin9066@gmail.com>
2022-04-20 16:46:01 +0900

    [추가] 프록시패턴과 데코레이터 패턴 코드 추가

--- 17. Spring Advanced/proxy/src/main/java/hello/proxy/app/v2/OrderServiceV2.java
+++ 17. Spring Advanced/proxy/src/main/java/hello/proxy/app/v2/OrderServiceV2.java
@@ -1,2 +1,14 @@
-package hello.proxy.app.v2;public class OrderServiceV2 {
+package hello.proxy.app.v2;
+
+import hello.proxy.app.v1.OrderRepositoryV1;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class OrderServiceV2 {
+
+	private final OrderRepositoryV2 orderRepository;
+
+	public void orderItem(String itemId) {
+		orderRepository.save(itemId);
+	}
 }
--- 17. Spring Advanced/proxy/src/main/java/hello/proxy/app/v3/OrderControllerV3.java
+++ 17. Spring Advanced/proxy/src/main/java/hello/proxy/app/v3/OrderControllerV3.java
@@ -1,2 +1,29 @@
-package hello.proxy.app.v3;public class OrderControllerV3 {
+package hello.proxy.app.v3;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import hello.proxy.app.v2.OrderServiceV2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class OrderControllerV3 {
+
+	private final OrderServiceV3 orderService;
+
+	@GetMapping("/v3/request")
+	public String request(String itemId) {
+		orderService.orderItem(itemId);
+		return "ok";
+	}
+
+	@GetMapping("/v3/no-log")
+	public String noLog() {
+		return "ok";
+	}
 }
