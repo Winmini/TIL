5f3a8442e4cc4e1f3d5ec98c08070ec383a89a3d
Seungmin Lim <seungmin9066@gmail.com>
2022-04-22 14:00:21 +0900

    [추가] 동적 프록시 테스트 코드 추가

---
+++ 17. Spring Advanced/proxy/src/test/java/hello/proxy/jdkdynamic/JdkDynamicProxyTest.java
@@ -0,0 +1,28 @@
+package hello.proxy.jdkdynamic;
+
+import java.lang.reflect.Proxy;
+
+import org.junit.jupiter.api.Test;
+
+import hello.proxy.jdkdynamic.code.AImpl;
+import hello.proxy.jdkdynamic.code.AInterface;
+import hello.proxy.jdkdynamic.code.TimeInvocationHandler;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class JdkDynamicProxyTest {
+
+	@Test
+	void dynamicA() {
+		AInterface target = new AImpl();
+
+		TimeInvocationHandler handler = new TimeInvocationHandler(target);
+
+		AInterface proxy = (AInterface)Proxy.newProxyInstance(AInterface.class.getClassLoader(), new Class[] {AInterface.class}, handler);
+
+		proxy.call();
+		log.info("target.getClass()={}", target.getClass());
+		log.info("proxy.getClass()={}", proxy.getClass());
+
+	}
+}
